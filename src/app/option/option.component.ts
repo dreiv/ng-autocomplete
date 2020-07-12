be75501de982c7e315ef82b3f820c09b14ac0753
Andrei Voicu <dreivoicu@gmail.com>
2020-07-12 07:54:59 +0300

    creared option component

---
+++ src/app/option/option.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-option',
+  templateUrl: './option.component.html',
+  styleUrls: ['./option.component.scss']
+})
+export class OptionComponent implements OnInit {
+  @Input() value: string;
+  click$: Observable<string>;
+
+  constructor(private host: ElementRef) {}
+
+  ngOnInit(): void {
+    this.click$ = fromEvent(this.element, 'click').pipe(mapTo(this.value));
+  }
+
+  get element(): HTMLElement {
+    return this.host.nativeElement;
+  }
+}
